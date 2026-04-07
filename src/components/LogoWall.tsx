0dfcf91cb8f7114df3271f30484bb190dfd4529c
dickycpy <141305157+dickycpy@users.noreply.github.com>
2026-04-07 18:20:56 +0800

    Update LogoWall.tsx

--- src/components/LogoWall.tsx
+++ src/components/LogoWall.tsx
@@ -1,28 +1,40 @@
 import { motion } from "motion/react";
 
-const brands = [
-  "Google", "Apple", "Meta", "Amazon", "Netflix", "Microsoft", "Adobe", "Spotify"
+const logos = [
+  "https://cdn.worldvectorlogo.com/logos/google-2015.svg",
+  "https://cdn.worldvectorlogo.com/logos/apple.svg",
+  "https://cdn.worldvectorlogo.com/logos/meta-1.svg",
+  "https://cdn.worldvectorlogo.com/logos/amazon-2.svg",
+  "https://cdn.worldvectorlogo.com/logos/netflix-3.svg",
+  "https://cdn.worldvectorlogo.com/logos/microsoft-5.svg",
+  "https://cdn.worldvectorlogo.com/logos/adobe-2.svg",
+  "https://cdn.worldvectorlogo.com/logos/spotify-1.svg"
 ];
 
 export default function LogoWall() {
   return (
     <div className="w-full overflow-hidden py-20 border-y border-black/5">
       <motion.div
-        className="flex whitespace-nowrap"
+        className="flex whitespace-nowrap items-center"
         animate={{ x: [0, -1000] }}
         transition={{
           repeat: Infinity,
-          duration: 20,
+          duration: 30,
           ease: "linear",
         }}
       >
-        {[...brands, ...brands, ...brands].map((brand, i) => (
-          <span
+        {[...logos, ...logos, ...logos].map((logo, i) => (
+          <div
             key={i}
-            className="text-4xl md:text-6xl font-bold tracking-tighter uppercase px-12 opacity-10"
+            className="px-16 opacity-20 grayscale hover:opacity-100 hover:grayscale-0 transition-all duration-500"
           >
-            {brand}
-          </span>
+            <img 
+              src={logo} 
+              alt="Brand Logo" 
+              className="h-8 md:h-12 w-auto object-contain"
+              referrerPolicy="no-referrer"
+            />
+          </div>
         ))}
       </motion.div>
     </div>
