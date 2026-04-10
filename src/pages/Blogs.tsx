f0d157c176f70647f93fe2cfb3962b4d749a42af
dickycpy <141305157+dickycpy@users.noreply.github.com>
2026-04-10 11:34:03 +0800

    Update Blogs.tsx

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,6 +1,6 @@
 export default function Blogs() {
   return (
-    <div className="pt-32 px-6 md:px-12 lg:px-24 pb-40">
+    <div className="pt-24 md:pt-32 px-6 md:px-12 lg:px-24 pb-40">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter mb-20">Blogs</h1>
       <p className="text-2xl text-neutral-500">Coming soon...</p>
     </div>
