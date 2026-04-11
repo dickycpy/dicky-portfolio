a6425eacc4fbc0f2a09ba9b2b99f8b04a97969e3
dickycpy <141305157+dickycpy@users.noreply.github.com>
2026-04-11 23:27:33 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="px-6 md:px-12 lg:px-24 py-20 border-t border-black/5 flex flex-col md:flex-row justify-between items-center gap-8 bg-white relative z-10">
-      <p className="text-sm opacity-40">© 2026 Dicky Chu's Portfolio. All rights reserved.</p>
+      <p className="text-sm opacity-40">© 2026 Dicky Chu's Portfolio. Made in Hong Kong 🇭🇰</p>
       <div className="flex gap-8">
         <a 
           href="https://www.linkedin.com/in/dicky-chu/" 
