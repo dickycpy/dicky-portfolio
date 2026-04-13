4c5e6f37ac67568c3e9854f3b8e4310e5325456d
dickycpy <141305157+dickycpy@users.noreply.github.com>
2026-04-13 19:31:41 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,22 @@
 export default function Footer() {
   return (
-    <footer className="px-6 md:px-12 lg:px-24 py-20 border-t border-black/5 flex flex-col md:flex-row justify-between items-center gap-8 bg-white relative z-10">
-      <p className="text-sm opacity-40">© 2026 Dicky Chu's Portfolio. Made in Hong Kong 🇭🇰</p>
-      <div className="flex gap-8">
-        <a 
-          href="https://www.linkedin.com/in/dicky-chu/" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="text-sm font-medium uppercase tracking-widest hover:opacity-60"
-        >
-          LinkedIn
-        </a>
+    <footer className="px-6 md:px-12 lg:px-24 py-20 border-t border-black/5 bg-white relative z-10">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-8">
+          <p className="text-sm opacity-40 text-center md:text-left">
+            © 2026 Dicky Chu's Portfolio. <br className="md:hidden" /> Made in Hong Kong 🇭🇰
+          </p>
+          <div className="flex gap-8">
+            <a 
+              href="https://www.linkedin.com/in/dicky-chu/" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="text-sm font-medium uppercase tracking-widest hover:opacity-60"
+            >
+              LinkedIn
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
